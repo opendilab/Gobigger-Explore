72eb2c3fde55ea19508cd51b268eee77976bcd2f
Xu Jingxin <sailxjx@gmail.com>
2022-01-10 11:46:02 +0800

    Revert batch size

--- my_submission/config/gobigger_no_spatial_config.py
+++ my_submission/config/gobigger_no_spatial_config.py
@@ -35,7 +35,7 @@ gobigger_config = dict(
         ),
         learn=dict(
             update_per_collect=8,
-            batch_size=128,
+            batch_size=256,
             learning_rate=0.001,
             target_theta=0.005,
             discount_factor=0.99,
